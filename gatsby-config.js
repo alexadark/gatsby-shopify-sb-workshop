370e1b849892c28a0fc0a51d9beadbd3758b87cd
Alexa Spalato <alexaspalato@gmail.com>
2022-10-10 15:50:23 +0200

    connect shopify to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,14 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+        password: process.env.SHOPIFY_ADMIN_API_ACCESS_TOKEN,
+        shopifyConnections: ["collections"], // source product collections too
+      },
+    },
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
