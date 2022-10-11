1fc46832722883e6871b1698abbab51ddacd5469
Alexa Spalato <alexaspalato@gmail.com>
2022-10-11 16:37:33 +0200

    correction

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -41,15 +41,15 @@ export const StoreProvider = ({ children }) => {
           quantity,
         },
       ];
-      const addItems = await client.checkout.addLineItems(
+      const newCheckout = await client.checkout.addLineItems(
         checkout.id,
         lineItems
       );
 
-      console.log("addItems", addItems);
+      console.log("newCheckout", newCheckout);
       //Buy now button
       //if we are in the browser we open the checkout page for this variant product
-      isBrowser && window.open(addItems.webUrl, "_blank");
+      isBrowser && window.open(newCheckout.webUrl, "_blank");
     } catch (error) {
       console.log(error);
     }
