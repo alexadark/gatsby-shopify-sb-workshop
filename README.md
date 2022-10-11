b8139da80f0f9accec8f1004e7b0147bb09645fb
Alexa Spalato <alexaspalato@gmail.com>
2022-10-11 10:41:36 +0200

    create single product

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Create Product Card
+# Create Single Product
 
-1. Create ui-components/Image
-2. Create the ProductCard component
-3. Add the ProductCard component to the Collection.js template - Import the component - add the fragment to the page query - Include the component in the Collection template
-4. Correct `tailwind.config.js` in order to include templates
+1. Create `pages/{shopifyProduct.handle}.js` using the [File System Route API](https://www.gatsbyjs.com/docs/reference/routing/file-system-route-api/)
---
+++ src/pages/{shopifyProduct.handle}.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "~/components/Layout";
+import { Image } from "~/components/ui-components/Image";
+
+const ProductPage = ({ data }) => {
+  const { title, description, variants, featuredImage } = data.shopifyProduct;
+
+  return (
+    <Layout>
+      <div className="container items-center gap-10 mx-auto my-10 product md:flex">
+        <div className="flex-1 product__image">
+          <Image img={featuredImage} />
+        </div>
+        <div className="flex-1 product__info">
+          <h1 className="mb-2 font-bold">{title}</h1>
+          <div className="mb-2 font-bold product__price">
+            {variants[0].price}eur
+          </div>
+          <div className="product__description">{description}</div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default ProductPage;
+
+export const pageQuery = graphql`
+  query ($handle: String!) {
+    shopifyProduct(handle: { eq: $handle }) {
+      title
+      description
+      featuredImage {
+        gatsbyImageData
+      }
+      variants {
+        price
+      }
+      featuredImage {
+        src
+        altText
+      }
+    }
+  }
+`;
